3af079f7101870d6a347d131f623c872fcba087d
Boris Karpov <gr-buratino@yandex.ru>
2022-04-04 19:23:01 +0300

    Fixed not named constant value 1e-6; Added numeric lib function accumulate;

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,10 +8,12 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double ACCURACY_THRESHOLD = 1e-6;
 
 string ReadLine() {
     string s;
@@ -86,7 +88,7 @@ public:
         auto matched_documents = FindAllDocuments(query, filter_condition);
         sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
-                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                if (abs(lhs.relevance - rhs.relevance) < ACCURACY_THRESHOLD) {
                     return lhs.rating > rhs.rating;
                 } else {
                     return lhs.relevance > rhs.relevance;
@@ -158,10 +160,7 @@ private:
         if (ratings.empty()) {
             return 0;
         }
-        int rating_sum = 0;
-        for (const int rating : ratings) {
-            rating_sum += rating;
-        }
+        int rating_sum = accumulate(rating.begin(), rating.end(), 0);
         return rating_sum / static_cast<int>(ratings.size());
     }
 
